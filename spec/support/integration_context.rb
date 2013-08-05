f0b88890c9bac990c7efe59af0dcfe0897b6544c
Sasha Gerrand <sasha.gerrand@bigcommerce.com>
2013-08-05 18:44:12 +1000

    Trailing newline all the things!

--- spec/support/integration_context.rb
+++ spec/support/integration_context.rb
@@ -3,7 +3,7 @@ shared_context "integration" do
     ENV['API_URL'] = ENV['API_URL'] || 'https://store-vnh06c71.mybigcommerce.com'
     ENV['API_USER'] = ENV['API_USER'] || 'apiuser'
     ENV['API_PASS'] = ENV['API_PASS'] || '123456'
-    
+
     Bigcommerce::Api.new({
       :store_url => ENV['API_URL'],
       :username => ENV['API_USER'],
